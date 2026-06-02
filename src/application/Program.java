7b204c6a9a77b1fe0605442f14bf446dad4099cf
Enzo Martins <menzo280@gmail.com>
2026-06-02 12:08:49 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -19,7 +20,7 @@ public class Program {
 		
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while(true) {
+		while (!chessMatch.getCheckMate()) {
 		try {
 			UI.clearScreen();
 			UI.printMatch(chessMatch, captured);
@@ -51,6 +52,8 @@ public class Program {
 			sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -60,15 +60,23 @@ public class UI {
 	}
 	
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
-		printBoard(chessMatch.getPieces());
-		System.out.println();
-		printCapturedPieces(captured);
-		System.out.println();
-		System.out.println("Turn :" + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
-		}
+	    printBoard(chessMatch.getPieces());
+	    System.out.println();
+	    printCapturedPieces(captured);
+	    System.out.println();
+	    System.out.println("Turn: " + chessMatch.getTurn());
+
+	    if (!chessMatch.getCheckMate()) {
+	        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+	        if (chessMatch.getCheck()) {
+	            System.out.println("CHECK!");
+	        }
+	    } 
+	    else {
+	        System.out.println("CHECKMATE!");
+	        System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+	    }
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
